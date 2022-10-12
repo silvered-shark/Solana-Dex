141b2676c9030e50f2ad6ed64f74143af4cce030
1228shark <fullstack1997@gmail.com>
2022-10-12 11:56:09 +0800

    Update lib.rs and tests to include more methods

---
+++ programs/Solana-Dex/src/lib.rs
@@ -0,0 +1,262 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token::{Mint, Token, TokenAccount, mint_to};
+use spl_token_swap::state::SwapVersion;
+// use spl_token_swap::processor::Processor;
+// use spl_token_swap::instruction::Initialize;
+use spl_token_swap::instruction::initialize;
+use spl_token_swap::curve::fees;
+use spl_token_swap::curve::base;
+use std::sync::Arc;
+use spl_token_swap::curve::constant_product::ConstantProductCurve;
+use anchor_lang::solana_program::program::invoke;
+
+
+
+declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
+
+#[program]
+pub mod solana_dex {
+
+
+    use super::*;
+
+    // create Token Swap State Account
+    // Create Token Mint Accounts
+    // Find swap pool Authority PDA
+    // Create ATAs
+    // Mint tokens into the ATAs
+    // Create Pool Token Mint Account, Pool Token Account, Pool Token Fee Account
+    // Create swap pool via CPI
+
+
+    pub fn initialize_token_swap_account(ctx: Context<InitializeTokenSwapAccount>) -> Result<()> {
+        msg!("Created Token Swap State Account---");
+        Ok(())
+    }
+
+    pub fn initialize_mint_accounts(ctx: Context<InitializeMintAccounts>) -> Result<()> {
+        msg!("Created Token X Mint Account---");
+        msg!("Created Token Y Mint Account---");
+        Ok(())
+    }
+
+    pub fn initialize_token_accounts(ctx: Context<InitializeTokenAccounts>) -> Result<()> {
+        msg!("Created Token X ATA---");
+        msg!("Created Token Y ATA---");
+        Ok(())
+    }
+
+    pub fn mint_tokens(ctx: Context<MintTokens>, amount: u64) -> Result<()> {
+        // Create the MintTo struct for our context
+        let cpi_accounts = anchor_spl::token::MintTo {
+            mint: ctx.accounts.mint_account.to_account_info(), 
+            to: ctx.accounts.token_account.to_account_info(), 
+            authority: ctx.accounts.mint_authority.to_account_info(), 
+        };
+        let cpi_program = ctx.accounts.token_program.to_account_info();
+        // Create the CpiContext we need for the request
+        let cpi_ctx = CpiContext::new(cpi_program, cpi_accounts);
+
+        // Execute anchor's helper function to mint tokens
+        mint_to(cpi_ctx, amount)?;
+        msg!("Minted {} tokens to ATA---", amount);
+        Ok(())
+    }
+
+    pub fn initialize_pool_accounts(ctx: Context<InitializePoolAccounts>) -> Result<()> {
+        msg!("Created Pool Token Mint Account---");
+        msg!("Created Pool Token Account---");
+        msg!("Created Pool Token Fee Account---");
+        Ok(())
+    }
+
+    pub fn initialize_swap_pool(ctx: Context<InitializeSwapPool>, bump:u8) -> Result<()> {
+
+        let swap_fees = fees::Fees { 
+            trade_fee_numerator: 0, 
+            trade_fee_denominator: 10000, 
+            owner_trade_fee_numerator: 5, 
+            owner_trade_fee_denominator: 10000, 
+            owner_withdraw_fee_numerator: 0, 
+            owner_withdraw_fee_denominator: 0, 
+            host_fee_numerator: 20, 
+            host_fee_denominator: 100 
+        };
+        let swap_curve = base::SwapCurve {
+            curve_type: base::CurveType::ConstantProduct, 
+            calculator:  Arc::new(ConstantProductCurve {})
+        };
+
+        let initialize_instruction = initialize(
+            ctx.accounts.token_swap_program.key, 
+            ctx.accounts.token_program.key, 
+            ctx.accounts.swap_pubkey.key, 
+            ctx.accounts.authority_pubkey.key, 
+            &ctx.accounts.token_x_ata.key(), 
+            &ctx.accounts.token_y_ata.key(), 
+            &ctx.accounts.pool_token_mint.key(), 
+            &ctx.accounts.pool_token_fee_account.key(), 
+            &ctx.accounts.pool_token_account.key(), 
+            swap_fees, 
+            swap_curve
+        )?;
+
+        msg!("Calling the token swap program to initialize a new pool...");
+        invoke(
+            &initialize_instruction,
+            &[
+                // 0. `[writable, signer]` New Token-swap to create.
+                ctx.accounts.token_swap_account.to_account_info(),
+                // 1. `[]` swap authority derived from `create_program_address(&[Token-swap account])`
+                ctx.accounts.swap_pubkey.clone(),
+                // 2. `[]` token_a Account. Must be non zero, owned by swap authority.
+                ctx.accounts.token_x_ata.to_account_info(),
+                // 3. `[]` token_b Account. Must be non zero, owned by swap authority.
+                ctx.accounts.token_y_ata.to_account_info(), 
+                // 4. `[writable]` Pool Token Mint. Must be empty, owned by swap authority.
+                ctx.accounts.pool_token_mint.to_account_info(), 
+                // 5. `[]` Pool Token Account to deposit trading and withdraw fees.
+                ctx.accounts.pool_token_fee_account.to_account_info(), 
+                // 6. `[writable]` Pool Token Account to deposit the initial pool token supply
+                ctx.accounts.pool_token_account.to_account_info(), 
+                // 7. `[]` Token program id
+                ctx.accounts.token_program.to_account_info()
+            ],
+        )?;
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct InitializeTokenSwapAccount<'info> {
+    #[account(mut)]
+    signer: Signer<'info>, 
+    #[account(
+        init, 
+        payer=signer, 
+        space=SwapVersion::LATEST_LEN, 
+        owner=token_swap_program.key()
+    )]
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    token_swap_account: AccountInfo<'info>, 
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    token_swap_program: AccountInfo<'info>, 
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeMintAccounts<'info> {
+    #[account(mut)]
+    signer: Signer<'info>, 
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    mint_authority: AccountInfo<'info>, 
+    #[account(
+        init, 
+        payer=signer, 
+        mint::decimals = 9, 
+        mint::authority = mint_authority
+    )]
+    x_mint: Account<'info, Mint>,
+    #[account(
+        init, 
+        payer=signer, 
+        mint::decimals = 9, 
+        mint::authority = mint_authority
+    )]
+    y_mint: Account<'info, Mint>,
+    token_program: Program<'info, Token>,
+    rent: Sysvar<'info, Rent>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeTokenAccounts<'info> {
+    #[account(mut)]
+    signer: Signer<'info>, // Signer
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    token_authority: AccountInfo<'info>, // Authority of the ATAs
+    x_mint: Account<'info, Mint>,  // X-mint
+    y_mint: Account<'info, Mint>,  // Y-mint
+    #[account(
+        init,
+        payer=signer,
+        token::mint = x_mint,
+        token::authority = token_authority,
+    )]
+    token_x_ata: Account<'info, TokenAccount>, // Token X ATA
+    #[account(
+        init,
+        payer=signer,
+        token::mint = y_mint,
+        token::authority = token_authority,
+    )]
+    token_y_ata: Account<'info, TokenAccount>, // Token Y ATA
+    token_program: Program<'info, Token>, // Token Program
+    rent: Sysvar<'info, Rent>,     // Rent
+    system_program: Program<'info, System>, // System Program
+}
+
+#[derive(Accounts)]
+pub struct MintTokens<'info> {
+    #[account(mut)]
+    mint_authority: Signer<'info>, 
+    #[account(mut)]
+    mint_account: Account<'info, Mint>,
+    #[account(mut)]
+    token_account: Account<'info, TokenAccount>,
+    token_program: Program<'info, Token>
+}
+
+#[derive(Accounts)]
+pub struct InitializePoolAccounts<'info> {
+    #[account(mut)]
+    signer: Signer<'info>, 
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    mint_authority: AccountInfo<'info>, 
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    fee_owner: AccountInfo<'info>, 
+    #[account(
+        init, 
+        payer=signer, 
+        mint::decimals = 9, 
+        mint::authority = mint_authority
+    )]
+    pool_token_mint: Account<'info, Mint>, 
+    #[account(
+        init,
+        payer=signer, 
+        token::mint = pool_token_mint, 
+        token::authority = signer
+    )]
+    pool_token_account: Account<'info, TokenAccount>, // Initial LP tokens 
+    #[account(
+        init, 
+        payer=signer, 
+        token::mint=pool_token_mint, 
+        token::authority=fee_owner
+    )]
+    pool_token_fee_account: Account<'info, TokenAccount>, // Swap Fees
+    token_program: Program<'info, Token>,
+    rent: Sysvar<'info, Rent>,
+    system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct InitializeSwapPool<'info> {
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    token_swap_program: AccountInfo<'info>, 
+    token_program: Program<'info, Token>,
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    swap_pubkey: AccountInfo<'info>, 
+    /// CHECK: This is not dangerous because we don't read or write from this account
+    authority_pubkey: AccountInfo<'info>, 
+    token_x_ata: Box<Account<'info, TokenAccount>>, // Token X ATA
+    token_y_ata: Box<Account<'info, TokenAccount>>, // Token Y ATA
+    #[account(mut)]
+    pool_token_mint: Box<Account<'info, Mint>>, 
+    pool_token_fee_account: Box<Account<'info, TokenAccount>>, // Swap Fees
+    #[account(mut)]
+    pool_token_account: Box<Account<'info, TokenAccount>>, // Initial LP tokens 
+    #[account(mut)]
+    token_swap_account: Signer<'info>,
+}
---
+++ tests/Solana-Dex.ts
@@ -0,0 +1,197 @@
+import * as anchor from "@project-serum/anchor";
+import * as splToken from "@solana/spl-token";
+import { Program } from "@project-serum/anchor";
+import NodeWallet from "@project-serum/anchor/dist/cjs/nodewallet";
+import { SolanaDex } from "../target/types/solana_dex";
+import { Commitment, Keypair, SYSVAR_RENT_PUBKEY } from "@solana/web3.js";
+import { TOKEN_SWAP_PROGRAM_ID } from "@solana/spl-token-swap";
+import { assert } from "chai";
+
+describe("Solana-Dex", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.SolanaDex as Program<SolanaDex>;
+
+  // const signer = (provider.wallet as NodeWallet).payer;
+  // console.log("Signer :: ", signer.publicKey.toString());
+
+  const owner = provider.wallet.publicKey;
+  const mintAuthority = anchor.web3.Keypair.generate();
+
+  const tokenSwapStateAccount = anchor.web3.Keypair.generate();
+
+  let x_mint;
+  let y_mint;
+  let token_x_ata;
+  let token_y_ata;
+
+  let pool_token_mint;
+  let swapAuthority;
+  let bump;
+
+  let pool_token_fee;
+  let pool_token_account;
+
+  it("Initialize token swap account", async () => {
+    const tx = await program.methods
+      .initializeTokenSwapAccount()
+      .accounts({
+        signer: owner,
+        tokenSwapAccount: tokenSwapStateAccount.publicKey,
+        tokenSwapProgram: TOKEN_SWAP_PROGRAM_ID,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([tokenSwapStateAccount])
+      .rpc();
+    console.log(
+      "Token Swap State Account :: " +
+        tokenSwapStateAccount.publicKey.toString()
+    );
+    // const commitment: Commitment = "confirmed";
+    const tokenSwapAccountInfo = await provider.connection.getAccountInfo(
+      tokenSwapStateAccount.publicKey
+    );
+    assert.equal(
+      tokenSwapAccountInfo.owner.toString(),
+      TOKEN_SWAP_PROGRAM_ID.toString()
+    );
+  });
+
+  it("Initialize Mint Accounts", async () => {
+    x_mint = anchor.web3.Keypair.generate();
+    y_mint = anchor.web3.Keypair.generate();
+    console.log("Mint Authority :: " + mintAuthority.publicKey.toString());
+    console.log("x_mint :: " + x_mint.publicKey.toString());
+    console.log("y_mint :: " + y_mint.publicKey.toString());
+    const tx = await program.methods
+      .initializeMintAccounts()
+      .accounts({
+        signer: owner,
+        mintAuthority: mintAuthority.publicKey,
+        xMint: x_mint.publicKey,
+        yMint: y_mint.publicKey,
+        tokenProgram: splToken.TOKEN_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([x_mint, y_mint])
+      .rpc();
+  });
+
+  it("Initialize swap pool Authority (PDA), and ATAs", async () => {
+    [swapAuthority, bump] = await anchor.web3.PublicKey.findProgramAddress(
+      [tokenSwapStateAccount.publicKey.toBuffer()],
+      TOKEN_SWAP_PROGRAM_ID
+    );
+    console.log("Swap Pool Authority :: " + swapAuthority.toString());
+
+    token_x_ata = anchor.web3.Keypair.generate();
+    console.log(`token_x_ata :: `, token_x_ata.publicKey.toString());
+
+    token_y_ata = anchor.web3.Keypair.generate();
+    console.log(`token_y_ata :: `, token_y_ata.publicKey.toString());
+
+    const tx = await program.methods
+      .initializeTokenAccounts()
+      .accounts({
+        signer: owner,
+        tokenAuthority: swapAuthority,
+        xMint: x_mint.publicKey,
+        yMint: y_mint.publicKey,
+        tokenXAta: token_x_ata.publicKey,
+        tokenYAta: token_y_ata.publicKey,
+        tokenProgram: splToken.TOKEN_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([token_x_ata, token_y_ata])
+      .rpc();
+  });
+
+  it("Mint Tokens to Token Accounts", async () => {
+    const tx = await program.methods
+      .mintTokens(new anchor.BN(100))
+      .accounts({
+        mintAuthority: mintAuthority.publicKey,
+        mintAccount: x_mint.publicKey,
+        tokenAccount: token_x_ata.publicKey,
+        tokenProgram: splToken.TOKEN_PROGRAM_ID,
+      })
+      .signers([mintAuthority])
+      .rpc();
+    const token_x_ata_info = await splToken.getAccount(
+      provider.connection,
+      token_x_ata.publicKey
+    );
+    console.log("token_x_ata balance :: " + token_x_ata_info.amount.toString());
+
+    const txn = await program.methods
+      .mintTokens(new anchor.BN(200))
+      .accounts({
+        mintAuthority: mintAuthority.publicKey,
+        mintAccount: y_mint.publicKey,
+        tokenAccount: token_y_ata.publicKey,
+        tokenProgram: splToken.TOKEN_PROGRAM_ID,
+      })
+      .signers([mintAuthority])
+      .rpc();
+    const token_y_ata_info = await splToken.getAccount(
+      provider.connection,
+      token_y_ata.publicKey
+    );
+    console.log("token_y_ata balance :: " + token_y_ata_info.amount.toString());
+  });
+
+  it("Initialize Pool Token Mint, Pool Token Account and Pool Token Fee Account", async () => {
+    pool_token_mint = anchor.web3.Keypair.generate();
+    console.log("pool_token_mint :: " + pool_token_mint.publicKey.toString());
+
+    pool_token_account = anchor.web3.Keypair.generate();
+    console.log(
+      "pool_token_account :: " + pool_token_account.publicKey.toString()
+    );
+
+    pool_token_fee = anchor.web3.Keypair.generate();
+    const fee_owner = new anchor.web3.PublicKey(
+      "HfoTxFR1Tm6kGmWgYWD6J7YHVy1UwqSULUGVLXkJqaKN"
+    );
+    console.log("pool_token_fee :: " + pool_token_fee.publicKey.toString());
+
+    const tx = await program.methods
+      .initializePoolAccounts()
+      .accounts({
+        signer: owner,
+        mintAuthority: swapAuthority,
+        feeOwner: fee_owner,
+        poolTokenMint: pool_token_mint.publicKey,
+        poolTokenAccount: pool_token_account.publicKey,
+        poolTokenFeeAccount: pool_token_fee.publicKey,
+        tokenProgram: splToken.TOKEN_PROGRAM_ID,
+        rent: SYSVAR_RENT_PUBKEY,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      })
+      .signers([pool_token_mint, pool_token_account, pool_token_fee])
+      .rpc();
+  });
+
+  it("Initialize Swap Pool", async () => {
+    const tx = await program.methods
+      .initializeSwapPool(bump)
+      .accounts({
+        tokenSwapProgram: TOKEN_SWAP_PROGRAM_ID,
+        tokenProgram: splToken.TOKEN_PROGRAM_ID,
+        swapPubkey: swapAuthority,
+        authorityPubkey: mintAuthority.publicKey,
+        tokenXAta: token_x_ata.publicKey,
+        tokenYAta: token_y_ata.publicKey,
+        poolTokenMint: pool_token_mint.publicKey,
+        poolTokenFeeAccount: pool_token_fee.publicKey,
+        poolTokenAccount: pool_token_account.publicKey,
+        tokenSwapAccount: tokenSwapStateAccount.publicKey,
+      })
+      .signers([tokenSwapStateAccount])
+      .rpc();
+    console.log("Successfully Initialized swap pool");
+  });
+});
